ce5a264dea3db6ae9a03bbb3da136d0e7dc3e02d
Enn Michael <enntheprogrammer@gmail.com>
2019-11-14 20:49:16 +0100

    Hope my Mom is proud

---
+++ src/main/java/com/publitio/jpublitio/JsonException.java
@@ -0,0 +1,11 @@
+package com.publitio.jpublitio;
+
+class JsonException extends Exception
+{
+    public static final long serialVersionUID = 53442;
+
+    public JsonException(String message)
+    {
+        super(message);
+    }
+}
