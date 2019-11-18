ec52bc73b10aead806a38358a832e451b732b7d4
Enn Michael <enntheprogrammer@gmail.com>
2019-11-18 11:19:10 +0100

    Now uploaded to maven central

---
+++ src/main/java/com/github/ennmichael/jpublitio/JsonException.java
@@ -0,0 +1,16 @@
+package com.github.ennmichael.jpublitio;
+
+/**
+ * This exception is thrown when parsing the response JSON fails.
+ * It might mean either that your endpoint path is wrong, or there
+ * was an internal server error.
+ */
+public class JsonException extends Exception
+{
+    public static final long serialVersionUID = 53442;
+
+    public JsonException(String message)
+    {
+        super(message);
+    }
+}
---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.github.ennmichael.jpublitio","l":"JsonException"},{"p":"com.github.ennmichael.jpublitio","l":"PublitioApi"}]
